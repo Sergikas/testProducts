cc8b43f24f0b1d0f5c4c975a23edc50c3f9a8f89
Sergio Carrillo <checho223076@hotmail.com>
2022-07-28 10:42:28 -0500

    added button for select amount of products

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,25 @@
 import Product from "./Product.js";
 import "./App.css";
-//import Data from "../Data.js";
-import {useState, useEffect} from "react";
+import Data from "../Data.js";
+//import {useState, useEffect} from "react";
 
 export default function App(){
 
-    const [dataProduct,setDataProduct] = useState([])
+    /* const [dataProduct,setDataProduct] = useState([])
 
     useEffect(()=>{
         async function getData(){
-            const res = await fetch("https://polite-sherbet-fd6ad4.netlify.app/api/products")
+            console.log("fetching data")
+            const res = await fetch("https://api.jikan.moe/v4/anime")
             const data = await res.json()
-            setDataProduct(data)
+            setDataProduct(data.data)
         }
         getData()
-    },[])
-
-    console.log(dataProduct)
+    },[]) */
 
     return(
         <div className="app-container">
-            {/*<Product data={dataProduct[0]}/>*/}
+            <Product data={Data[0]} key={Data[0].name}/>
         </div>
     )
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,34 @@
 import "./Product.css";
 import {useState} from "react";
+import minus from "../images/icons/icon-minus.svg";
+import plus from "../images/icons/icon-plus.svg";
+import cart from "../images/icons/icon-cart.svg";
 
 export default function Product(props){
 
-    const [focusImage,setFocusImage] = useState(props.data.images[0])
+    const [focusImage,setFocusImage] = useState(props.data.images[0]);
+    const [amountProduct,setAmountProduct] = useState(0);
+
+    // Display the corrent amount of avalible products (no more than storage either minus of 0)
+    function handleAmountProducts(operation){
+        operation==="minus" ? 
+
+        setAmountProduct(prevState => {
+            if(prevState-1 >= 0){
+                return prevState - 1;
+            }else{
+                return 0;
+            }}
+        ) : 
+        setAmountProduct(prevState =>{
+            if(prevState+1 <= props.data.storage){
+                return prevState + 1;
+            }else{
+                alert("No se cuenta con mas stock")
+                return prevState
+            }
+        });
+    }
     
     function handleFocusImage(image){
         setFocusImage(image);
@@ -27,7 +52,15 @@ export default function Product(props){
                 <p className="product-detail">{props.data.description}</p>
                 <h2 className="product-price">${props.data.price}</h2>
                 <div className="product-buttons">
-                        <button className="button-product-1 buttons"></button>
+                        <button className="button-product-1 buttons">
+                            <button className="btn-controll btn1" onClick={()=>handleAmountProducts("minus")}>
+                                <img src={minus} alt="" />
+                            </button>
+                            {amountProduct}
+                            <button className="btn-controll btn2" onClick={()=>handleAmountProducts("plus")}>
+                                <img src={plus} alt="" />
+                            </button>
+                        </button>
                         <button className="button-product-2 buttons">Add to car</button>
                 </div>
             </div>
